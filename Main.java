4cb47305f0567b8bd10eda1dad92efb563f75241
Jarek <101186879+jarek167714@users.noreply.github.com>
2022-04-05 11:36:25 +0000

    Final

--- Main.java
+++ Main.java
@@ -10,15 +10,20 @@ class Main
           System.out.print(' ');
         for(int j = i; j > 0; j--)
           System.out.print('*');
-      }
+    
+          System.out.println(' ');
+        }
+    
+    
   for (int i = 1; i <= Wysokosc; i++)
     {
       System.out.print("     ");
       for(int j = 1; j <= i; j++)
         {
           System.out.print('*');
-          System.out.println(' ');
+
         }
+          System.out.println(' ');
     }
   }
 }
