e2229243bafce9ef273e7bdb758cb6541818679a
jarek167714 <101186879+jarek167714@users.noreply.github.com>
2022-04-05 11:29:39 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    int Wysokosc = 4;
+
+    for(int i = 1; i <= Wysokosc; i++)
+      {
+        for(int j = Wysokosc; j >= i; j--)
+          System.out.print(' ');
+        for(int j = i; j > 0; j--)
+          System.out.print('*');
+      }
+  for (int i = 1; i <= Wysokosc; i++)
+    {
+      System.out.print("     ");
+      for(int j = 1; j <= i; j++)
+        {
+          System.out.print('*');
+          System.out.println(' ');
+        }
+    }
+  }
+}
+    
